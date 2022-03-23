4ccdcce8cadd97b0e5418abc40cb446903f6ee2c
jegatheesprana <jegatheesprana@gmail.com>
2022-03-24 00:10:01 +0530

    card added

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -1,14 +1,5 @@
 import React, { useMemo, useState } from "react"
-import {
-    Navbar,
-    Container,
-    Row,
-    Col,
-    DropdownButton,
-    Dropdown,
-    Button,
-    Card,
-} from "react-bootstrap"
+import { Container, Row, Col, Card } from "react-bootstrap"
 import { measurments, colors } from "../config"
 import moment from "moment"
 
@@ -62,118 +53,123 @@ const CurrentTime = ({
     // console.log(dateNOW)
 
     return (
-        <Container
+        <Card
             fluid
             className={`mt-${measurments.componentSpacing} py-2 bg-dark text-white`}
         >
-            <Row>
-                <Col>
-                    <h6>Current Time</h6>
-                    {times.map((val, id) => {
-                        return (
-                            <Row className="px-4 mb-2" key={id}>
-                                <Col lg="5">
-                                    <Card
-                                        style={{
-                                            width: "5rem",
-                                            height: "2.5rem",
-                                            backgroundColor: "#c4c4c4",
-                                        }}
-                                    ></Card>
-                                </Col>
-                                <Col lg="7" className="mt-1">
-                                    <span className="align-middle">{val}</span>
-                                </Col>
-                            </Row>
-                        )
-                    })}
-                </Col>
-                <Col sm={9}>
-                    <Row>
-                        <Col sm={3} className="mt-2">
-                            <Row
-                                style={{
-                                    color: `${colors.currentTime.subColor}`,
-                                    fontWeight: "900",
-                                }}
-                            >
-                                <h6>
-                                    Upcoming/Current
-                                    <br />
-                                    Job Run
-                                </h6>
-                            </Row>
-                        </Col>
-                        <Col sm={3} className="mt-2">
-                            <Row
-                                style={{
-                                    color: `${colors.currentTime.subColor}`,
-                                    fontWeight: "bold",
-                                }}
-                            >
-                                <h6>
-                                    Expected completion
-                                    <br />
-                                    Time
-                                </h6>
-                            </Row>
-                        </Col>
-                        <Col sm={3} className="mt-2">
-                            <Row
-                                style={{
-                                    color: `${colors.currentTime.subColor}`,
-                                    fontWeight: "bold",
-                                }}
-                            >
-                                <h6>Time to Completion</h6>
-                            </Row>
-                        </Col>
-                        <Col sm={3} className="mt-2">
-                            <Row
-                                style={{
-                                    color: `${colors.currentTime.subColor}`,
-                                    fontWeight: "bold",
-                                }}
-                            >
-                                <h6>Number of Tasks</h6>
-                            </Row>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Row>
-                                <h5>
-                                    {data[selectedDate + 1]?.["Run time"]} EST
-                                </h5>
-                            </Row>
-                        </Col>
-                        <Col>
-                            <Row>
-                                <h5>{expectedCompletion} EST</h5>
-                            </Row>
-                        </Col>
-                        <Col>
-                            <Row>
-                                <h5>
-                                    {
-                                        data[selectedDate]?.[
-                                            "Total duration(expected)"
-                                        ]
-                                    }
-                                </h5>
-                            </Row>
-                        </Col>
-                        <Col>
-                            <Row>
-                                <h5>
-                                    {data[selectedDate]?.["number of taks"]}
-                                </h5>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </Container>
+            <Card.Body>
+                <Row>
+                    <Col>
+                        <h6>Current Time</h6>
+                        {times.map((val, id) => {
+                            return (
+                                <Row className="px-4 mb-2" key={id}>
+                                    <Col lg="5">
+                                        <Card
+                                            style={{
+                                                width: "5rem",
+                                                height: "2.5rem",
+                                                backgroundColor: "#c4c4c4",
+                                            }}
+                                        ></Card>
+                                    </Col>
+                                    <Col lg="7" className="mt-1">
+                                        <span className="align-middle">
+                                            {val}
+                                        </span>
+                                    </Col>
+                                </Row>
+                            )
+                        })}
+                    </Col>
+                    <Col sm={9}>
+                        <Row>
+                            <Col sm={3} className="mt-2">
+                                <Row
+                                    style={{
+                                        color: `${colors.currentTime.subColor}`,
+                                        fontWeight: "900",
+                                    }}
+                                >
+                                    <h6>
+                                        Upcoming/Current
+                                        <br />
+                                        Job Run
+                                    </h6>
+                                </Row>
+                            </Col>
+                            <Col sm={3} className="mt-2">
+                                <Row
+                                    style={{
+                                        color: `${colors.currentTime.subColor}`,
+                                        fontWeight: "bold",
+                                    }}
+                                >
+                                    <h6>
+                                        Expected completion
+                                        <br />
+                                        Time
+                                    </h6>
+                                </Row>
+                            </Col>
+                            <Col sm={3} className="mt-2">
+                                <Row
+                                    style={{
+                                        color: `${colors.currentTime.subColor}`,
+                                        fontWeight: "bold",
+                                    }}
+                                >
+                                    <h6>Time to Completion</h6>
+                                </Row>
+                            </Col>
+                            <Col sm={3} className="mt-2">
+                                <Row
+                                    style={{
+                                        color: `${colors.currentTime.subColor}`,
+                                        fontWeight: "bold",
+                                    }}
+                                >
+                                    <h6>Number of Tasks</h6>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Row>
+                                    <h5>
+                                        {data[selectedDate + 1]?.["Run time"]}{" "}
+                                        EST
+                                    </h5>
+                                </Row>
+                            </Col>
+                            <Col>
+                                <Row>
+                                    <h5>{expectedCompletion} EST</h5>
+                                </Row>
+                            </Col>
+                            <Col>
+                                <Row>
+                                    <h5>
+                                        {
+                                            data[selectedDate]?.[
+                                                "Total duration(expected)"
+                                            ]
+                                        }
+                                    </h5>
+                                </Row>
+                            </Col>
+                            <Col>
+                                <Row>
+                                    <h5>
+                                        {data[selectedDate]?.["number of taks"]}
+                                    </h5>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
     )
 }
 

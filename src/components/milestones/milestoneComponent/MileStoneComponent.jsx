f129ab02e76e2ed4749d73c78522a6d3d57b1a6e
Thakshayan <thakshayan007@gmail.com>
2022-03-23 23:46:17 +0530

    popover

--- src/components/milestones/milestoneComponent/MileStoneComponent.jsx
+++ src/components/milestones/milestoneComponent/MileStoneComponent.jsx
@@ -1,9 +1,20 @@
-import { Card, Row, Col } from "react-bootstrap"
+import { Card, Row, Col, Button,DropdownButton,Dropdown,OverlayTrigger,Tooltip,Popover,Overlay } from "react-bootstrap"
 import { colors, measurments } from "../../../config"
 import Detail from "./Detail"
 import Stepper from "./Stepper"
+import { useRef,useState } from "react"
 
 const MileStoneComponent = ({stage}) => {
+
+    const [show, setShow] = useState(false);
+    const [target, setTarget] = useState(null);
+    const ref = useRef(null);
+
+    const handleClick = (event) => {
+        setShow(!show);
+        setTarget(event.target);
+    };
+
     return (
         <Card
             bg="dark"
@@ -45,7 +56,57 @@ const MileStoneComponent = ({stage}) => {
                         </Row>
                     </Col>
                     <Col xs={1} sm={1} className="pt-3 px-4 text-end">
-                        <i className="bi bi-three-dots-vertical"></i>
+                        
+                        {/* <DropdownButton
+                            className="dropdown-toggle"
+                            id="dropdown-button-drop-start"
+                            drop="start"
+                            menuVariant="dark"
+                            title={<i className="bi bi-three-dots-vertical"></i>}
+                            variant="dark"
+                        >
+                           
+                            <Dropdown.Menu variant="dark">
+                                <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                            </Dropdown.Menu>
+                            
+                        </DropdownButton> */}
+                        
+                        {/* <OverlayTrigger
+                            placement="left"
+                            overlay={<Tooltip id="button-tooltip-2">Check out this avatar</Tooltip>}
+                        >
+                            {({ ref, ...triggerHandler }) => (
+                            <Button
+                                variant="dark"
+                                {...triggerHandler}
+                                className="d-inline-flex align-items-center"
+                            >
+                               <i className="bi bi-three-dots-vertical" ref={ref}></i>
+                            </Button>
+                            )}
+                        </OverlayTrigger> */}
+
+                    <div ref={ref}>
+                        <Button onFocus={handleClick} onBlur={handleClick}  variant="dark">
+                            <i className="bi bi-three-dots-vertical"></i>
+                        </Button>
+
+                        <Overlay
+                            show={show}
+                            target={target}
+                            placement="left"
+                            container={ref}
+                            containerPadding={20}
+                        >
+                            <Popover id="popover-contained" >
+                            {/* <Popover.Header as="h3"  >Popover bottom</Popover.Header> */}
+                            <Popover.Body>
+                                <strong>Popover bottom</strong> 
+                            </Popover.Body>
+                            </Popover>
+                        </Overlay>
+                    </div>
                     </Col>
                 </Row>
                 <Row>
